536f94955b618cae2b1ea0b36ccd2c289160f687
shatyuka <shatyuka@qq.com>
2025-08-04 02:01:41 +0800

    fix: missing user profile java for mac

--- Cation/Core/Java/JavaFinder/MacJavaFinder.cs
+++ Cation/Core/Java/JavaFinder/MacJavaFinder.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Cation.Core.Java.JavaFinder;
 
@@ -7,6 +9,22 @@ public class MacJavaFinder : IJavaFinder
 {
     public IEnumerable<string> Find()
     {
-        return !OperatingSystem.IsMacOS() ? [] : Utils.FindJavaBinPathRecurse("/Library/Java/JavaVirtualMachines");
+        if (!OperatingSystem.IsMacOS())
+            return [];
+
+        var searchDirs = new[]
+        {
+            "/Library/Java/JavaVirtualMachines",
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                "Library/Java/JavaVirtualMachines")
+        };
+        return (from baseDir in searchDirs
+            where Directory.Exists(baseDir)
+            from dir in Directory.GetDirectories(baseDir)
+            select Path.Combine(dir, "Contents", "Home", "bin")
+            into binPath
+            let javaPath = Path.Combine(binPath, JavaManager.JavaExecutableName)
+            where File.Exists(javaPath)
+            select binPath).ToList();
     }
 }
